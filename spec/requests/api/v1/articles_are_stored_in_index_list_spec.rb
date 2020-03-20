b11180c81bae7c4f9f8311efc792d94efd7cc870
Hunter <hmvitous@yahoo.com>
2020-03-20 15:18:14 +0100

    Creates route for article index

---
+++ spec/requests/api/v1/articles_are_stored_in_index_list_spec.rb
@@ -0,0 +1,19 @@
+describe 'GET /articles', type: :request do
+  describe'successfull' do
+    before do
+      get '/api/v1/articles'
+    end
+
+    it 'returns 200 status' do
+      expect(response.status).to eq 200
+    end
+  
+  
+  
+  
+  end
+
+
+
+
+end

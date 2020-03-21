4e0158bf40a3dfbd0bbf3cbe4be653a3220faa60
Hunter <hmvitous@yahoo.com>
2020-03-21 11:25:05 +0100

    Adds to test to see if factory bot is creating articles

--- spec/requests/api/v1/articles_are_stored_in_index_list_spec.rb
+++ spec/requests/api/v1/articles_are_stored_in_index_list_spec.rb
@@ -1,5 +1,6 @@
 describe 'GET /articles', type: :request do
   describe'successfull' do
+    let!(:articles) {5.times {create(:article)}}
     before do
       get '/api/v1/articles'
     end
@@ -7,5 +8,11 @@ describe 'GET /articles', type: :request do
     it 'returns 200 status' do
       expect(response.status).to eq 200
     end
+
+    it 'returns correct number of articles' do
+      expect(JSON.parse(response.body)["articles"].count).to eq 5
+    end
+
+
   end
 end

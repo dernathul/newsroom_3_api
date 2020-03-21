c0ec28df695b88b6f85bc5a04a7cc4aff1a4dcec
Craft Academy <craftacademy@Crafts-MacBook-Air.local>
2020-03-21 17:20:29 +0100

    creates article route and configures the model. Creates test for article route

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::ArticlesController < ApplicationController
     articles = Article.all
     render json: { articles: articles },status: 200
   end
+
+  def create
+    article = Article.new
+    render json: { article: article }, status: 200
+  end
+
 end
--- spec/requests/api/v1/articles_can_be_created_spec.rb
+++ spec/requests/api/v1/articles_can_be_created_spec.rb
@@ -1,1 +1,18 @@
-describe 'POST /article'
+describe 'POST /article', type: :request do
+  describe 'successfull' do
+    before do
+      post '/api/v1/articles'
+      @article = Article.new(title: "No more room in space", snippet: "Its all gone, sorry", content: "Govenor says this aint good")
+    end
+
+    it 'returns 200 status' do
+      expect(response.status).to eq 200
+    end
+
+    it 'title is not blank' do
+      binding.pry
+      expect(@article.title).to eq "No more room in space"
+    end
+  end
+  
+end

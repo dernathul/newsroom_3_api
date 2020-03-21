4022d09be3b8c6515d4fe9186730f2abc1ea8950
Hunter <hmvitous@yahoo.com>
2020-03-21 11:14:59 +0100

    Adds a snippet to articles

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
-    binding.pry
     articles = Article.all
     render json: { articles: articles },status: 200
   end
-  binding.pry
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :article do
     title { "MyString" }
+    snippet { "MySnippet" }
     content { "MyText" }
   end
 end

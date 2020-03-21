4ebd0d9fdaf5c3e6b4a5acaef4313855d7730b86
Hunter <hmvitous@yahoo.com>
2020-03-21 14:10:54 +0100

    Formats code to be nicer to look at

--- spec/requests/api/v1/articles_are_stored_in_index_list_spec.rb
+++ spec/requests/api/v1/articles_are_stored_in_index_list_spec.rb
@@ -1,7 +1,9 @@
 describe 'GET /articles', type: :request do
   describe'successfull' do
+    
     let!(:articles) {create(:article)}
     let!(:articles2) {create(:article, title: 'NOSPACE', snippet: "You thought you liked space", content: "NOSPACE is where you want to be")}
+    
     before do
       get '/api/v1/articles'
     end
@@ -9,6 +11,7 @@ describe 'GET /articles', type: :request do
     it 'returns 200 status' do
       expect(response.status).to eq 200
     end
+    
     it 'returns correct number of articles' do
       binding.pry
       expect(JSON.parse(response.body)["articles"].count).to eq 2

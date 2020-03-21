c3246f611953073675479e77a28053553ccf2f7b
Hunter <hmvitous@yahoo.com>
2020-03-21 11:10:47 +0100

    Renders the json file for articles

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
     binding.pry
-    @articles = Article.all
-
+    articles = Article.all
+    render json: { articles: articles },status: 200
   end
+  binding.pry
 end

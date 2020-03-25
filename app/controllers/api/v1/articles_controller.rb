0be44b0d5fcd9dea1e193894a7e5ddb842903924
Janko <mailodjanka@gmail.com>
2020-03-25 09:39:21 +0100

    Trying to wrap an article key in article params

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ArticlesController < ApplicationController
+  
   def index
     articles = Article.all
     render json: { articles: articles },status: 200
@@ -6,17 +7,17 @@ class Api::V1::ArticlesController < ApplicationController
 
   def create
     article = Article.create(article_params)
-    
+    binding.pry
     if article.persisted? 
       render json: { message: 'Your article was saved' }
     else 
-      render json: { message: article.errors.full_messages.to_sentence}, status: 422
+      render json: { error: article.errors.full_messages.to_sentence}, status: 422
     end
   end
 
   private
   
   def article_params
-    params.permit(:title, :snippet, :content)
+    params.require(:article).permit(:title, :snippet, :content)
   end
 end

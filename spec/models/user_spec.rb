61c9012a19476639ab3bfb640039253c8efaf480
emtalen <59577366+emtalen@users.noreply.github.com>
2020-03-27 09:45:27 +0100

    installing devise token auth and writes spec for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe User, type: :model do
+  describe "DB table" do
+    it { is_expected.to have_db_column :encrypted_password }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :tokens }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :password }
+
+    context "should not have an invalid email adress" do
+      emails = ["asdf@ ds.com", "@example.com", "test me @yahoo.com",
+                "asdf@example", "ddd@.d. .d", "ddd@.d"]
+
+      emails.each do |email|
+        it { is_expected.not_to allow_value(email).for(:email) }
+      end
+    end
+
+    context "should have valied email address" do
+      emails = ["asdf@ds.com", "hello@example.uk", "test1234@yahoo.si",
+                "asdf@example.eu"]
+
+      emails.each do |email|
+        it { is_expected.to allow_value(email).for(:email) }
+      end
+    end
+
+    describe "Factory" do
+      it "should have valid Factory" do
+        expect(FactoryBot.create(:user)).to be_valid
+      end
+    end
+  end
+end

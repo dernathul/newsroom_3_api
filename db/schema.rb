cf4a063d7d324c0d340c0da98597027c46283235
emtalen <59577366+emtalen@users.noreply.github.com>
2020-03-28 19:14:15 +0100

    adds atribute premium to articles as boolean and sets default value to true

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_125544) do
+ActiveRecord::Schema.define(version: 2020_03_28_181213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "content"
     t.text "snippet"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category"
+    t.boolean "premium", default: true
   end
 
 end

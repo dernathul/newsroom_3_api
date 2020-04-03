ae9720fd7e7432b855047c8afb0d0023deb34fe8
Janko <mailodjanka@gmail.com>
2020-04-03 14:44:14 +0200

    Adds editor to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,4 @@ Article.create title: 'Sci-Fi is the new Rom-Com', snippet: 'Scientists say that
 User.create email: 'journalist@mail.com', password: 'password', role: 'journalist'
 User.create email: 'user@mail.com', password: 'password', role: 'reg_user'
 User.create email: 'subscriber@mail.com', password: 'password', role: 'subscriber'
+User.create email: 'editor@mail.com', password: 'password', role: 'editor'

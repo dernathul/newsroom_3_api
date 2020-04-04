94be9004d00cf89d9ffb4a2df38cbc7fc4b13ff2
Janko <mailodjanka@gmail.com>
2020-04-04 17:08:56 +0200

    Formatts the code

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,26 +1,23 @@
 RSpec.describe Article, type: :model do
-  describe 'DB table' do
+  describe "DB table" do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :title }
-    it { is_expected.to have_db_column :snippet}
-    it { is_expected.to have_db_column :content}
-    it { is_expected.to have_db_column :category}
-    it { is_expected.to have_db_column :premium}
-    it { is_expected.to have_db_column :published}
+    it { is_expected.to have_db_column :snippet }
+    it { is_expected.to have_db_column :content }
+    it { is_expected.to have_db_column :category }
+    it { is_expected.to have_db_column :premium }
+    it { is_expected.to have_db_column :published }
   end
-  
-  describe 'Validations' do
+
+  describe "Validations" do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :snippet }
-    it { is_expected.to validate_presence_of :content}
-    it { is_expected.to validate_presence_of :category}
-    
-    
-    
+    it { is_expected.to validate_presence_of :content }
+    it { is_expected.to validate_presence_of :category }
   end
-  
-  describe 'Factory' do
-    it 'should have valid Factory' do
+
+  describe "Factory" do
+    it "should have valid Factory" do
       expect(FactoryBot.create(:article)).to be_valid
     end
   end
